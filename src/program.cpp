5d43a6665c168d2bb5914e00fe8f571dfb77a5f4
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-06-09 21:51:42 +0200

    Some improvements

--- src/program.cpp
+++ src/program.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <tensorflow/lite/version.h>
+#include <tensorflow/lite/interpreter.h>
 
 int main() {
     std::cout << "Using tensorflow version: " << TFLITE_VERSION_STRING << std::endl;
+    std::unique_ptr<tflite::Interpreter> interpreter;
+    if (interpreter == nullptr) {
+        std::cerr << "Failed to create TensorFlow Lite interpreter." << std::endl;
+        return -1;
+    }
     return 0;
 }
