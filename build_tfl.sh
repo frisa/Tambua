67a0a88885715fe165d1e8dbf2bf624e3938096a
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-07-05 19:56:42 +0200

    Code cleanup

--- build_tfl.sh
+++ build_tfl.sh
@@ -7,35 +7,40 @@ CLEAN=1
 
 cd $ROOT_DIR
 if [ $CLONE -eq 1 ]; then
-    rm -fr tensorflow_src
+    rm -fr tflite_src
     git clone https://github.com/tensorflow/tensorflow.git tensorflow_src
 fi
 
 if [ $CLEAN -eq 1 ]; then
-    rm -fr tensorflow_build
-    rm -fr tensorflow_install
-    mkdir tensorflow_build
+    rm -fr tflite_build
+    rm -fr tflite_install
+    mkdir tflite_build
 fi
 
-cd tensorflow_build
-cmake ../tensorflow_src/tensorflow/lite \
-    -DTFLLITE_ENABLE_INSTALL=ON \
+cd tflite_build
+cmake ../tflite_src/tensorflow/lite \
     -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON \
     -DSYSTEM_FARMHASH=OFF \
-    -DSYSTEM_PTHREADPOOL=ON \
+    -DSYSTEM_PTHREADPOOL=OFF \
     -DABSL_PROPAGATE_CXX_STD=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/tensorflow_install \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DTFLITE_ENABLE_INSTALL=ON \
+    -DTFLITE_ENABLE_LABEL_IMAGE=OFF \
     -DTFLITE_ENABLE_RUY=OFF \
+    -DTFLITE_ENABLE_RESOURCE=OFF \
     -DTFLITE_ENABLE_NNAPI=OFF \
     -DTFLITE_ENABLE_GPU=OFF \
+    -DTFLITE_ENABLE_METAL=OFF \
     -DTFLITE_ENABLE_XNNPACK=OFF \
+    -DTFLITE_ENABLE_EXTERNAL_DELEGATES=OFF \
+    -DTFLITE_KERNEL_TEST=OFF \
     -DTFLITE_ENABLE_MMAP=OFF \
     -Wno-dev
 
-make -j $(nproc)
-make install
+cmake --build . -j
+cmake --install .
 exit 0
 cd $ROOT_DIR
 rm -fr build
