2a593c6eab6c1177da3dd64531e3351d07b19d12
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-07-07 14:43:34 +0200

    Building interpreter

--- src/program.cpp
+++ src/program.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <tensorflow/lite/version.h>
 #include <tensorflow/lite/interpreter.h>
 
 int main() {
-    std::cout << "Using tensorflow version: " << TFLITE_VERSION_STRING << std::endl;
     std::unique_ptr<tflite::Interpreter> interpreter;
     if (interpreter == nullptr) {
         std::cerr << "Failed to create TensorFlow Lite interpreter." << std::endl;
