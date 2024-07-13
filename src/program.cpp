7f8b6f418e71ea7a54f39fdf28cb7ccb93cfeeb8
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-07-13 21:27:50 +0200

    Building

--- src/program.cpp
+++ src/program.cpp
@@ -1,6 +1,7 @@
 #include "../include/program.h"
 #include "../include/log.h"
 #include "../include/bmp.h"
+#include <queue>
 
 #include <iostream>
 #include "tensorflow/lite/interpreter_builder.h"
@@ -9,6 +10,9 @@
 #include "tensorflow/lite/model_builder.h"
 #include "tensorflow/lite/optional_debug_tools.h"
 #include "tensorflow/lite/tools/delegates/delegate_provider.h"
+
+void get_top_n(uint8_t *prediction, int prediction_size, size_t num_results,
+               float threshold, std::vector<std::pair<float, int>> *top_results);
 
 int main(int argc, char *argv[])
 {
@@ -44,7 +48,7 @@ int main(int argc, char *argv[])
     const std::vector<int> outputs = interpreter->outputs();
     std::cout << "Number of inputs: " << inputs.size() << std::endl;
     std::cout << "Number of outputs: " << outputs.size() << std::endl;
-    
+
     // Get provided delegates based on params
     tflite::tools::ToolParams delegate_params;
     tflite::tools::ProvidedDelegateList delegate_list_util(&delegate_params);
@@ -53,7 +57,7 @@ int main(int argc, char *argv[])
     delegate_list_util.AddAllDelegateParams();
     delegates = delegate_list_util.CreateAllRankedDelegates();
     std::cout << "Number of delegates: " << delegates.size() << std::endl;
-    for (auto& delegate : delegates)
+    for (auto &delegate : delegates)
     {
         const auto name = delegate.provider->GetName();
         std::cout << "Delegate: " << name << std::endl;
@@ -71,14 +75,63 @@ int main(int argc, char *argv[])
     TFL_OK(interpreter->AllocateTensors());
 
     // Fill the input tensor
-    TfLiteIntArray* dims = interpreter->tensor(input)->dims;
+    TfLiteIntArray *dims = interpreter->tensor(input)->dims;
     int wanted_height = dims->data[1];
     int wanted_width = dims->data[2];
     int wanted_channels = dims->data[3];
     TfLiteType wanted_type = interpreter->tensor(input)->type;
     resize_bmp(interpreter->typed_tensor<uint8_t>(input), bmp_bytes.data(),
-                      height, width, channels, wanted_height, wanted_width, wanted_channels);
+               height, width, channels, wanted_height, wanted_width, wanted_channels);
 
+    // Run the interpreter
+    interpreter->Invoke();
+
+    // Get the output tensor
+    const float threshold = 0.001f;
+    std::vector<std::pair<float, int>> top_results;
+    int output = interpreter->outputs()[0];
+    int number_of_results = 5;
+    TfLiteIntArray *output_dims = interpreter->tensor(output)->dims;
+    auto output_size = output_dims->data[output_dims->size - 1];
+    get_top_n(interpreter->typed_output_tensor<uint8_t>(0),
+              output_size, number_of_results, threshold,
+              &top_results);
+    for (const auto &result : top_results)
+    {
+        const float confidence = result.first;
+        const int index = result.second;
+        std::cout << confidence << ": " << index << " " << std::endl;
+    }
     interpreter.reset();
     return 0;
+}
+
+void get_top_n(uint8_t *prediction, int prediction_size, size_t num_results,
+               float threshold, std::vector<std::pair<float, int>> *top_results)
+{
+    std::priority_queue<std::pair<float, int>, std::vector<std::pair<float, int>>,
+                        std::greater<std::pair<float, int>>>
+        top_result_pq;
+
+    const long count = prediction_size;
+    float value = 0.0;
+    for (int i = 0; i < count; ++i)
+    {
+        value = prediction[i] / 255.0;
+        if (value < threshold)
+        {
+            continue;
+        }
+        top_result_pq.push(std::pair<float, int>(value, i));
+        if (top_result_pq.size() > num_results)
+        {
+            top_result_pq.pop();
+        }
+    }
+    while (!top_result_pq.empty())
+    {
+        top_results->push_back(top_result_pq.top());
+        top_result_pq.pop();
+    }
+    std::reverse(top_results->begin(), top_results->end());
 }
