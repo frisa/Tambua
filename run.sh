6a10a520214c46469c532ecdc646066ca27f8cdd
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-07-20 04:46:17 +0200

    Cleanup code

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ HOME_DIR=$(echo ~)
 conan --version
 
 rm -fr $ROOT_DIR/build
-#rm -fr ~/.conan2/p
+rm -fr ~/.conan2/p
 
 rm -f $HOME_DIR/.conan2/profiles/ubuntu-gcc11-release
 rm -f $HOME_DIR/.conan2/profiles/ubuntu-gcc11-debug
@@ -15,10 +15,10 @@ cp $ROOT_DIR/conan/ubuntu-gcc11-release $HOME_DIR/.conan2/profiles/
 cp $ROOT_DIR/conan/ubuntu-gcc11-debug $HOME_DIR/.conan2/profiles/
 
 conan install conanfile.py --build=missing --profile=ubuntu-gcc11-debug --profile:build=ubuntu-gcc11-release
-conan install conanfile.py --build=missing --profile=ubuntu-gcc11-release --profile:build=ubuntu-gcc11-release
+#conan install conanfile.py --build=missing --profile=ubuntu-gcc11-release --profile:build=ubuntu-gcc11-release
 
 cmake --preset conan-debug
 cmake --build build --preset conan-debug
 
-cmake --preset conan-release
-cmake --build build --preset conan-release
+#cmake --preset conan-release
+#cmake --build build --preset conan-release
