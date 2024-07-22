805510bf5c997d37a7220b6e93fa9c9bbbb56c3f
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-07-22 10:57:26 +0200

    Workspace cleanup

--- Scripts/install_preset.sh
+++ Scripts/install_preset.sh
@@ -8,6 +8,15 @@ if ( [ "$preset" != "linux-release" ] && [ "$preset" != "linux-debug" ] ); then
     exit 1
 fi
 
+conan_version=$(conan --version)
+if [ "$conan_version" != "Conan version 2.5.0" ]; then
+    virtualenv venv
+    source venv/bin/activate
+    pip install conan==2.5.0
+    echo "Conan version 2.5.0 installed"
+    conan --version
+fi
+
 rm -fr $root_dir/build/$preset
 #rm -fr ~/.conan2/p
 
